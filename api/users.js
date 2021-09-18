20b6c15d2d576e14fee119ef5976d71f6025fe36
Jimmy Dang <dangjimmy84@gmail.com>
2021-09-17 23:00:58 -0700

    finished part 2

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
-const { getAllUsers, getUserByUsername } = require("../db");
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
 const jwt = require("jsonwebtoken");
 const {JWT_SECRET} = process.env
 
@@ -48,5 +48,41 @@ userRouter.post("/login", async (req, res, next) => {
     next(error);
   }
 });
+
+userRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
+  });
 
 module.exports = userRouter;
