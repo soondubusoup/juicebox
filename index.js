18b0d7dc2876f3376c6a4f83895b4e23d77c0eee
Jimmy Dang <dangjimmy84@gmail.com>
2021-09-18 18:03:31 -0700

    please

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 console.log(process.env.JWT_SECRET);
 
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env 
 const express = require('express');
 const server = express();
 
